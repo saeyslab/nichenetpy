bfb3f5df962f9c32d658b9dd5f07a17f38971341
Victor Medaerts <victor.medaerts@skynet.be>
2024-12-20 11:43:43 +0100

    one can never have enough documentation

--- src/nichenetpy/extraction.py
+++ src/nichenetpy/extraction.py
@@ -6,7 +6,24 @@ from anndata import AnnData
 import scanpy as sc
 
 
-def get_expressed_genes(celltype:str|list[str], ann:AnnData, pct:float=0.1) -> list[int]:
+def get_expressed_genes(celltype:str|list[str], ann:AnnData, pct:float=0.1) -> list[str]:
+    '''
+    Gets the expressed genes from an AnnData object. 
+
+    Parameters
+    ----------
+    celltype : str or list of str
+        the cell types to consider
+    ann : AnnData
+        the AnnData object to extract expressed genes from
+    pct : float
+        the minimum percent difference between the percent of cells expressing the gene in the cluster and the percent of cells expressing the gene in all other clusters combined.
+    
+    Returns
+    -------
+    list
+        list of expressed genes
+    '''
     if type(celltype) is str:
         celltype = [celltype]
     cells_oi = list(ann.obs.loc[[ct in celltype for ct in ann.obs["celltype"]]].index)
@@ -30,7 +47,31 @@ def get_expressed_genes(celltype:str|list[str], ann:AnnData, pct:float=0.1) -> l
         if val > pct
     ]
 
-def subset_ann_celltype(ann:AnnData, celltype:str|list[str], layers:list[str]=None, celltype_col:str="celltype"):
+def subset_ann_celltype(
+        ann:AnnData,
+        celltype:str|list[str],
+        layers:list[str]=None,
+        celltype_col:str="celltype"
+    ) -> AnnData:
+    '''
+    Subsets an AnnData object by cell type. 
+
+    Parameters
+    ----------
+    ann : AnnData
+        the AnnData object to subset
+    celltype : str or list of str
+        the cell types to subset by
+    layers : list of str or None
+        the layers to subset (additionally to subsetting obs), if None all layers are subsetted
+    celltype_col : str
+        the name of the column in obs that contains the cell types
+    
+    Returns
+    -------
+    AnnData
+        the subsetted AnnData object
+    '''
     if layers is None:
         layers = ann.layers.keys()
     cells_oi = ann.obs.loc[[ct in celltype for ct in ann.obs[celltype_col]]]
@@ -49,6 +90,25 @@ def get_weighted_ligand_receptor_links(
     lr_network:LigandReceptorNetwork,
     lr_sig:WeightedNetwork
 ) -> WeightedNetwork:
+    '''
+    Get the weighted ligand-receptor links between a possible ligand and its receptors. 
+
+    Parameters
+    ----------
+    best_upstream_ligands : list of str
+        the ligands of interest
+    expressed_receptors : list of str
+        the receptors expressed in the cell type of interest
+    lr_network : LigandReceptorNetwork
+        the ligand-receptor network containing the ligand-receptor interactions
+    lr_sig : WeightedNetwork
+        a weighted network containing the ligand-receptor interactions and their weights
+    
+    Returns
+    -------
+    WeightedNetwork
+        the weighted ligand-receptor links
+    '''
     lr_sig = lr_sig.subset(set(lr_network))
     best_upstream_ligands = set(best_upstream_ligands)
     expressed_receptors = set(expressed_receptors)
@@ -58,14 +118,41 @@ def get_weighted_ligand_receptor_links(
 def get_lfc_celltype(
     ann:AnnData,
     celltype:str,
-    condition_colname:str,
+    condition_col:str,
     condition_oi:str,
     condition_ref:str,
     layer:str,
-    celltype_coll:str="celltype",
+    celltype_col:str="celltype",
     features:list[str]=None
 ) -> list[float]:
-    ann_sender = subset_ann_celltype(ann, celltype, layers=[layer], celltype_col=celltype_coll)
+    '''
+    Get log fold change of genes between two conditions in cell type of interest from an AnnData object.
+
+    Parameters
+    ----------
+    ann : AnnData
+        the AnnData object
+    celltype : str
+        the cell type of interest
+    condition_colname : str
+        the name of the column in obs that contains the condition
+    condition_oi : str
+        the condition of interest
+    condition_ref : str
+        the reference condition
+    layer : str
+        the name of the data layer
+    celltype_col : str
+        the name of the column in obs that contains the cell types
+    features : list of str or None
+        the ligands to consider
+    
+    Returns
+    -------
+    WeightedNetwork
+        the weighted ligand-receptor links
+    '''
+    ann_sender = subset_ann_celltype(ann, celltype, layers=[layer], celltype_col=celltype_col)
     if features is not None:
         gene2index = dict(zip(ann.var["gene"], range(len(ann.var["gene"]))))
         ids = sorted(gene2index[gene] for gene in features)
@@ -82,7 +169,7 @@ def get_lfc_celltype(
     sc.pp.log1p(ann_sender, layer=layer)
     sc.tl.rank_genes_groups(
         ann_sender,
-        groupby=condition_colname,
+        groupby=condition_col,
         method="wilcoxon",
         layer=layer,
         groups=[condition_oi],
