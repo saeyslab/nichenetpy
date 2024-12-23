ccf9d6c60e70ca090fdd389b46287ee12773d114
Victor Medaerts <victor.medaerts@skynet.be>
2024-12-23 10:50:47 +0100

    get_expressed_genes_test.py

---
+++ tests/get_expressed_genes_test.py
@@ -0,0 +1,22 @@
+from nichenetpy.extraction import get_expressed_genes
+from nichenetpy.utils import read_list_from_csv
+
+import anndata
+import os
+
+
+root = os.path.dirname(__file__)
+
+def template_expressed_genes(celltype, ann, pct, exp):
+    res = set(get_expressed_genes(celltype, ann, pct))
+    assert len(res) == len(exp), f"expected {len(exp)} expressed genes, got {len(res)}"
+    for gene in exp:
+        assert gene in res, f"expected {gene} in expressed genes"
+
+def test_expressed_genes_0():
+    template_expressed_genes(
+        "CD8 T",
+        anndata.io.read_h5ad(os.path.join(root, "data/AnnData/anndata_2000_50.h5")),
+        0.05,
+        read_list_from_csv(os.path.join(root, "data/expressed_genes_receiver/0/exp_genes.csv"))
+    )
