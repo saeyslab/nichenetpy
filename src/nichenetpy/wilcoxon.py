b0d3dc93a73cda77aa0d8d98928c9ad8a2a9d814
Victor Medaerts <victor.medaerts@skynet.be>
2025-01-29 16:28:29 +0100

    _rank_cells with cellnames (needs optimization)

--- src/nichenetpy/wilcoxon.py
+++ src/nichenetpy/wilcoxon.py
@@ -1,25 +1,45 @@
 from scipy.sparse import csc_matrix
 
+from itertools import chain
+
 
 def _rank_cells(
-    mat:csc_matrix
+    mat:csc_matrix,
+    cells:list[str]
 ):
     if type(mat) is not csc_matrix:
         raise TypeError(f"mat should have type scipy.csc_matrix, was {type(mat)}")
     output = []
+    mat_cells = []
     nrows, ncols = mat.shape
     for ci in range(ncols):
         ranks = []
-        col = mat[:, ci]
+        indices_non_zero = mat.indices[mat.indptr[ci]:mat.indptr[ci+1]]
+        values_non_zero = mat.data[mat.indptr[ci]:mat.indptr[ci+1]]
+        cells_sorted, non_zero = zip(
+            *sorted(
+                zip(
+                    (cells[i] for i in indices_non_zero),
+                    values_non_zero
+                ),
+                key=lambda x : x[1]
+            )
+        )
+        n_zero = nrows - (mat.indptr[ci+1] - mat.indptr[ci])
         n_neg = 0
-        n_zero = nrows
-        for e in col.data:
-            if e < 0:
-                n_neg += 1
-            n_zero -= 1
+        while n_neg < len(non_zero) and non_zero[n_neg] < 0:
+            n_neg += 1
+        mat_cells.append(
+            list(
+                chain(
+                    cells_sorted[:n_neg],
+                    (cells[i] for i in range(nrows) if i not in indices_non_zero),
+                    cells_sorted[n_neg:]
+                )
+            )
+        )
         # compute average using gaussian summation
         zero_rank = n_neg + (n_zero - 1)/2
-        non_zero = sorted(col.data)
         i = 0
         while i < n_neg:
             n_tied = 1
@@ -43,4 +63,4 @@ def _rank_cells(
                 ranks.append(rank)
             i += n_tied
         output.append(ranks)
-    return output
+    return (mat_cells, output)
