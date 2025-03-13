42327e1b0ee455bfb9d1721bf79895ad20091c72
Victor Medaerts <victor.medaerts@skynet.be>
2025-03-13 16:53:23 +0100

    scale_quantile by column and WIP ligand_activity_single_cell.ipynb

--- src/nichenetpy/normalization.py
+++ src/nichenetpy/normalization.py
@@ -71,7 +71,11 @@ def _scale_quantile_seq(data:list|tuple|np.ndarray, cutoff:float=0.05) -> np.nda
         output = (data - sub) / div
     return np.clip(output, 0, 1)
 
-def scale_quantile(data:list|tuple|np.ndarray|pd.Series, cutoff:float=0.05) -> np.ndarray:
+def scale_quantile(
+    data:list|tuple|np.ndarray|pd.Series,
+    cutoff:float=0.05,
+    by_row:bool=False
+) -> np.ndarray:
     '''
     Cut off outer quantiles and rescale to a [0, 1] range. 
 
@@ -81,6 +85,8 @@ def scale_quantile(data:list|tuple|np.ndarray|pd.Series, cutoff:float=0.05) -> n
         the data to normalize
     cutoff : float
         the quantile cutoff for outliers
+    by_row : bool
+        indicates whether to apply the computation on each row or on each column
     
     Returns
     -------
@@ -93,26 +99,36 @@ def scale_quantile(data:list|tuple|np.ndarray|pd.Series, cutoff:float=0.05) -> n
         if the arguments have the wrong type
     ValueError
         if data is invalid
-    
-    Note
-    ----
-    Contrary to the R function dplyr::scale_quantile, this function is implemented row-by-row
     '''
     if not isinstance(cutoff, Number):
         raise TypeError(f"cutoff should have type float, was {type(cutoff)}")
+    if type(by_row) is not bool:
+        raise TypeError(f"by_row should have type bool, was {type(by_row)}")
     if type(data) is pd.Series:
         data = data.to_numpy()
     if type(data) is list or type(data) is tuple:
         if len(data) > 0:
             if type(data[0]) is list or type(data[0]) is tuple:
-                return np.concatenate([_scale_quantile_seq(row, cutoff=cutoff).reshape(1, -1) for row in data], axis=0)
+                if by_row:
+                    return np.concatenate([_scale_quantile_seq(row, cutoff=cutoff).reshape(1, -1) for row in data], axis=0)
+                else:
+                    return np.concatenate([_scale_quantile_seq(col, cutoff=cutoff).reshape(-1, 1) for col in zip(*data)], axis=1)
             elif type(data[0]) is float or type(data[0]) is int:
                 return _scale_quantile_seq(data, cutoff=cutoff)
             else:
                 raise ValueError(f"data should be a 1-dimensional or 2-dimensional array of numbers")
     elif type(data) is np.ndarray:
         if len(data.shape) == 2:
-            return np.concatenate([_scale_quantile_seq(data[i, :], cutoff=cutoff).reshape(1, -1) for i in range(data.shape[0])], axis=0)
+            if by_row:
+                return np.concatenate(
+                    [_scale_quantile_seq(data[i, :], cutoff=cutoff).reshape(1, -1) for i in range(data.shape[0])],
+                    axis=0
+                )
+            else:
+                return np.concatenate(
+                    [_scale_quantile_seq(data[:, i], cutoff=cutoff).reshape(-1, 1) for i in range(data.shape[1])],
+                    axis=1
+                )
         elif len(data.shape) == 1:
             return _scale_quantile_seq(data, cutoff=cutoff)
         else:
@@ -120,7 +136,11 @@ def scale_quantile(data:list|tuple|np.ndarray|pd.Series, cutoff:float=0.05) -> n
     else:
         raise TypeError(f"data should be of type list, tuple, numpy.ndarray or pandas.Series, was {type(data)}")
 
-def scale_quantile_adapted(data:list|tuple|np.ndarray|pd.Series, cutoff=0) -> np.ndarray:
+def scale_quantile_adapted(
+    data:list|tuple|np.ndarray|pd.Series,
+    cutoff=0,
+    by_row:bool=False
+) -> np.ndarray:
     '''
     Normalize values in a vector by quantile scaling. Add a pseudovalue of 0.001 to avoid having a score of 0 for the lowest value.
 
@@ -130,6 +150,8 @@ def scale_quantile_adapted(data:list|tuple|np.ndarray|pd.Series, cutoff=0) -> np
         the data to normalize
     cutoff : float
         the quantile cutoff for outliers
+    by_row : bool
+        indicates whether to apply the computation on each row or on each column
     
     Returns
     -------
@@ -142,12 +164,8 @@ def scale_quantile_adapted(data:list|tuple|np.ndarray|pd.Series, cutoff=0) -> np
         if data has the wrong type
     ValueError
         if data is invalid
-    
-    Note
-    ----
-    Contrary to the R function nichenetr::scale_quantile_adapted, this function is implemented row-by-row
     '''
-    return scale_quantile(data, cutoff=cutoff) + 0.001
+    return scale_quantile(data, cutoff=cutoff, by_row=by_row) + 0.001
 
 def scaling_zscore(data:list[float]) -> list[float]:
     '''
