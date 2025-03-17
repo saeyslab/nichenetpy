fixed scaling_modified_zscore (scaling factor)

src/nichenetpy/normalization.py
@@ -1,5 +1,5 @@
 from scipy.sparse import lil_matrix, csc_matrix, csr_matrix
-
+from scipy.stats import median_abs_deviation
 from numbers import Number
 
 import numpy as np
@@ -167,22 +167,75 @@ def scale_quantile_adapted(
     '''
     return scale_quantile(data, cutoff=cutoff, by_row=by_row) + 0.001
 
-def scaling_zscore(data:list[float]) -> list[float]:
+def scaling_zscore(data:list[float]|np.ndarray) -> list[float]|np.ndarray:
     '''
     Normalize values in a vector by the z-score method.
 
     Parameters
     ----------
-    data : list of float
+    data : list of float or numpy.ndarray
         the data to normalize
     
     Returns
     -------
-    list of float
+    list of float or numpy.ndarray
         the normalized data
+    
+    Raises
+    ------
+    TypeError
+        if the arguments have the wrong type
     '''
-    if len(data) == 1:
-        return [0]
-    sd = np.std(data)
-    avg = np.mean(data)
-    return [(x - avg) / sd for x in data] if sd > 0 else [x - avg for x in data]
+    if type(data) is list:
+        if len(data) == 1:
+            return [0]
+        sd = np.std(data)
+        avg = np.mean(data)
+        return [(x - avg) / sd for x in data] if sd > 0 else [x - avg for x in data]
+    elif type(data) is np.ndarray:
+        if len(data) == 1:
+            return np.array([0])
+        sd = np.std(data)
+        avg = np.mean(data)
+        return (data - avg) / sd if sd > 0 else data - avg
+    else:
+        raise TypeError(f"data should have type list[float] or numpy.ndarray, was {type(data)}")
+
+def scaling_modified_zscore(
+    data:list[float]|np.ndarray,
+    scale_factor:float=0.6744907594765952
+) -> list[float]|np.ndarray:
+    '''
+    Normalize values in a vector by the z-score method.
+
+    Parameters
+    ----------
+    data : list of float or numpy.ndarray
+        the data to normalize
+    
+    Returns
+    -------
+    list of float or numpy.ndarray
+        the normalized data
+    
+    Raises
+    ------
+    TypeError
+        if the arguments have the wrong type
+    '''
+    if type(data) is list:
+        md = np.median(data)
+        if median_abs_deviation(data, nan_policy="omit", scale=scale_factor) == 0:
+            return [0.6745 * (x - md) for x in data]
+        else:
+            mad = median_abs_deviation(data, scale=scale_factor)
+            return [0.6745 * (x - md) / mad for x in data]
+    elif type(data) is np.ndarray:
+        md = np.median(data)
+        if median_abs_deviation(data, nan_policy="omit", scale=scale_factor) == 0:
+            return 0.6745 * (data - md)
+        else:
+            mad = median_abs_deviation(data, scale=scale_factor)
+            return 0.6745 * (data - md) / mad
+    else:
+        raise TypeError(f"data should have type list[float] or numpy.ndarray, was {type(data)}")