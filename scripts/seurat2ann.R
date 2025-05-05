27c2616f7bd7ba7e29b436c748a840a45d5a4b0e
Victor Medaerts <victor.medaerts@skynet.be>
2025-05-05 17:54:51 +0200

    started differentiation_example.ipynb

--- scripts/seurat2ann.R
+++ scripts/seurat2ann.R
@@ -3,21 +3,25 @@ library("Seurat")
 library("SeuratObject")
 library("hdf5r")
 setwd('D:/Data/nichenetpy')
-old <- readRDS("./seurat/seuratObj3531889.rds")
-
-seuratObj <- CreateSeuratObject(
-  counts = GetAssayData(old, layer="counts"),
-  data = GetAssayData(old, layer="data"),
-  meta.data = old@meta.data
-)
-seuratObj <- SetAssayData(
-  seuratObj,
-  layer="scale.data",
-  new.data=GetAssayData(old, layer="scale.data")
-)
-seuratObj@assays[["RNA"]]@layers$counts@Dimnames <- old@assays[["RNA"]]$counts@Dimnames
-seuratObj@assays[["RNA"]]@layers$data@Dimnames <- old@assays[["RNA"]]$data@Dimnames
+old <- readRDS("./seurat/seurat_obj_subset_integrated_zonation.rds")
+seuratObj <- old
+seuratObj$RNA <- ScaleData(old$RNA)
+seuratObj$SCT <- ScaleData(old$SCT)
+seuratObj <- PrepSCTFindMarkers(seuratObj, assay = "SCT")
+#seuratObj <- CreateSeuratObject(
+#  counts = GetAssayData(old, layer="counts"),
+#  data = GetAssayData(old, layer="data"),
+#  meta.data = old@meta.data
+#)
+#seuratObj <- SetAssayData(
+#  seuratObj,
+#  layer="scale.data",
+#  new.data=GetAssayData(old, layer="scale.data")
+#)
+#seuratObj@assays[["RNA"]]@layers$counts@Dimnames <- old@assays[["RNA"]]$counts@Dimnames
+#seuratObj@assays[["RNA"]]@layers$data@Dimnames <- old@assays[["RNA"]]$data@Dimnames
 seuratObj[["RNA"]]@meta.data$gene <- old@assays[["RNA"]]$counts@Dimnames[[1]]
+seuratObj[["SCT"]]@meta.data$gene <- old@assays[["SCT"]]$counts@Dimnames[[1]]
 ann <- anndataR::from_Seurat(
   seuratObj,
   "InMemoryAnnData"
@@ -25,5 +29,6 @@ ann <- anndataR::from_Seurat(
 #anndataR::from_Seurat(
 #  seuratObj,
 #  "HDF5AnnData",
-#  file="./annData/annData3531889.h5"
+#  file="./annData/seurat_obj_subset_integrated_zonation.h5",
+#  mode="w"
 #)
