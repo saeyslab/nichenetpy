dd6c39114af7a62e812ac846eaf8b0cca7f11fbf
Victor Medaerts <victor.medaerts@skynet.be>
2025-07-23 14:06:54 +0200

    significantly faster wilcoxon test

--- src/nichenetpy/wilcoxon.py
+++ src/nichenetpy/wilcoxon.py
@@ -19,7 +19,7 @@ def _rank_cells(
 ):
     if type(cell_groups) is pd.Series:
         # indexing series is slow and deprecated (warning is thrown)
-        cell_groups = list(cell_groups)
+        cell_groups = tuple(cell_groups)
     if type(mat) is not csc_matrix:
         try:
             mat = csc_matrix(mat)
